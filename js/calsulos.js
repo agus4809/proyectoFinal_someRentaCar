90bff2d1ac6ccfccdb8ddd588bf50810611dffd3
augusto Garcia <augusto4809@gmail.com>
2022-06-16 20:46:29 -0300

    commit de todos los archivos

---
+++ js/calsulos.js
@@ -0,0 +1,7 @@
+function calculoDias(){
+    
+    var salida = document.querySelector("#salida").value;
+    var llegada = document.querySelector("#llegada").value;
+    alert(llegada + salida)
+
+}
