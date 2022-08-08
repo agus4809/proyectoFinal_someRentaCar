e2d7d993148fc81675973f55a2862e81a023b5ed
augusto Garcia <augusto4809@gmail.com>
2022-08-07 22:49:56 -0300

    ultima actualizacion entrega final

--- js/calsulos.js
+++ js/calsulos.js
@@ -1,14 +1,39 @@
-const fechas = document.querySelector("#formularioFechas");
+// // const fechas = document.querySelector("#formularioFechas");
 
 
-fechas.addEventListener("submit", mostrarFechas())
+// // fechas.addEventListener("submit", mostrarFechas())
 
-function mostrarFechas(e){
-    e.preventDefault();
-    const inicio = document.querySelector("#formGroupExampleInput").value;
-    const fin = document.querySelector("#formGroupExampleInput2").value;
-    console.log("validando" + " " + inicio + " " + fin)
-}
+// // function mostrarFechas(e){
+// //     e.preventDefault();
+// //     const inicio = document.querySelector("#formGroupExampleInput").value;
+// //     const fin = document.querySelector("#formGroupExampleInput2").value;
+// //     console.log("validando" + " " + inicio + " " + fin)
+// // }
+// function calcFecha(){
+//   var f1= new Date(document.getElementById("formGroupExampleInput").value);
+//   var f2= new Date(document.getElementById("formGroupExampleInput2").value);
+//   var dif = (f2.getTime() - f1.getTime())/(1000*60*60*24);
+//   var hoy = new Date ();
+
+//   if(f1 < hoy){
+//     alert("la fecha no puede ser menor a la del dia de hoy")
+    
+//   }
+  
+//   // var onix = 6950;
+//   // var prisma = 7680;
+//   // var spin = 10125;
+//   // var cruze = 9180;
+
+//   // var precioOnix = onix*dif;
+//   // var precioPrisma = prisma*dif;
+//   // var preciospin = spin*dif;
+//   // var precioCruze = cruze*dif;
+  
+
+
+
+// }
 
 
 
